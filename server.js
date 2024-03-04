78ffbd7edfb2cbf10ae472c0dd173deb4b0edc0a
ncalihu0 <144197067+ncalihu0@users.noreply.github.com>
2024-03-04 12:22:37 -0500

    corrected admin from true/false to binary

--- server.js
+++ server.js
@@ -126,8 +126,11 @@ app.get('/login', (req, res) => {
             return res.status(500).json({ error: 'Internal server error' })
         } else {
             if (data.length > 0) {
-                if (data[0].password === password && data[0].admin === true) {
-                    return res.status(200).json({ redirect: '/admin' });
+                if (data[0].password === password && data[0].admin === 1) {
+                    return res.status(200).json({
+                        redirect: '/admin',
+                        message: `Welcome back ${data[0].first_name}`
+                    });
                 } else if (data[0].password === password) {
                     return res.status(200).json({ message: `Welcome back ${data[0].first_name}` });
                 } else {
